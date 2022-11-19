21b15cf6cebd23ed0645a0da07134335e7b3532d
DOriginalSamxie <samadrufai@gmail.com>
2022-11-19 15:00:37 +0100

    fiNALLY God did it

--- pages/index.js
+++ pages/index.js
@@ -47,9 +47,7 @@ export default function Home() {
             className={styles.card}
           >
             <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <p>GOD MAKE THIS POSSIBLE.</p>
           </a>
         </div>
       </main>
